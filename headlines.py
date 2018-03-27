3a314c99070ac91a5d63e6660faf7128a81dadc0
Jorgen Ordonez <jordonez@speend-wireless.com>
2018-03-27 15:18:04 -0600

    Using GET

--- headlines.py
+++ headlines.py
@@ -4,19 +4,24 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-PS_FEED = {'hacking': 'https://rss.packetstormsecurity.com/news/tags/hacking',
-           'dos': 'https://rss.packetstormsecurity.com/files/tags/denial_of_service/',
-           'code_exec': 'https://rss.packetstormsecurity.com/files/tags/code_execution/',
-           'encryption': 'https://rss.packetstormsecurity.com/files/tags/encryption/',
-           'exploit': 'https://rss.packetstormsecurity.com/files/tags/exploit/'}
+RSS_FEED = {'hacking': 'https://rss.packetstormsecurity.com/news/tags/hacking',
+            'dos': 'https://rss.packetstormsecurity.com/files/tags/denial_of_service/',
+            'code_exec': 'https://rss.packetstormsecurity.com/files/tags/code_execution/',
+            'encryption': 'https://rss.packetstormsecurity.com/files/tags/encryption/',
+            'exploit': 'https://rss.packetstormsecurity.com/files/tags/exploit/'}
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="hacking"):
-    feed = feedparser.parse(PS_FEED[publication])
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEED:
+        publication = "hacking"
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEED[publication])
     return render_template("home.html",articles=feed['entries'])
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
