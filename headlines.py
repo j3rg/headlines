c46deb3d3285f6abdf3717be6372393c2129b3bc
Jorgen Ordonez <jordonez@speend-wireless.com>
2018-05-04 11:08:21 -0600

    Added weather information

--- headlines.py
+++ headlines.py
@@ -2,6 +2,9 @@
 # coding=utf-8
 
 import feedparser
+import json
+import urllib2
+import urllib
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -14,6 +17,21 @@ RSS_FEED = {'hacking': 'https://rss.packetstormsecurity.com/news/tags/hacking',
             'encryption': 'https://rss.packetstormsecurity.com/files/tags/encryption/',
             'exploit': 'https://rss.packetstormsecurity.com/files/tags/exploit/'}
 
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=97931e3b253b78925314710b42497ae0"
+    query = urllib.quote(query)
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":
+                   parsed["weather"][0]["description"],
+                   "temperature":parsed["main"]["temp"],
+                   "city":parsed["name"]
+                   }
+        return weather
+
 @app.route("/", methods=['GET','POST'])
 def get_news():
     query = request.form.get("publication")
@@ -22,6 +40,7 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEED[publication])
-    return render_template("home.html",articles=feed['entries'])
+    weather = get_weather("Belize")
+    return render_template("home.html",articles=feed['entries'],weather=weather)
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
